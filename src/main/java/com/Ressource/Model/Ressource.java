0298edc24516f679e1511b75959e89628b63d41d
lahcen404 <lahcen.maskour2003@gmail.com>
2025-03-17 15:11:54 +0000

    Create Resource Model

--- src/main/java/com/Ressource/Model/Ressource.java
+++ src/main/java/com/Ressource/Model/Ressource.java
@@ -1,4 +1,63 @@
 package com.Ressource.Model;
 
 public class Ressource {
+    private int id;
+    private String name;
+    private String type;
+    private int quantity;
+
+    public Ressource(int id, String name, String type, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.quantity = quantity;
+    }
+
+    public Ressource(String name, String type, int quantity) {
+        this.name = name;
+        this.type = type;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Ressource{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
