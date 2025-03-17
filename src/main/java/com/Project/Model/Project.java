7a93e4a6ceac3f5e7a747d14ab30848d6853208e
lahcen404 <lahcen.maskour2003@gmail.com>
2025-03-17 15:06:58 +0000

    Create Project Model

--- src/main/java/com/Project/Model/Project.java
+++ src/main/java/com/Project/Model/Project.java
@@ -1,4 +1,89 @@
 package com.Project.Model;
 
+import java.util.Date;
+
 public class Project {
+    private int id;
+    private String name;
+    private String description;
+    private Date start_date;
+    private Date end_date;
+    private double budget;
+
+    public Project(int id, String name, String description, Date start_date, Date end_date, double budget) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.start_date = start_date;
+        this.end_date = end_date;
+        this.budget = budget;
+    }
+
+    public Project(String name, String description, Date start_date, Date end_date, double budget) {
+        this.name = name;
+        this.description = description;
+        this.start_date = start_date;
+        this.end_date = end_date;
+        this.budget = budget;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(Date end_date) {
+        this.end_date = end_date;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", start_date=" + start_date +
+                ", end_date=" + end_date +
+                ", budget=" + budget +
+                '}';
+    }
 }
